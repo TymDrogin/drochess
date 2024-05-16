affb6106b7b17fbca73adbfdeca3ec4ba0566019
Tym Drogin <tim.drogin@gmail.com>
2024-05-17 00:17:37 +0100

    Added thiserror mod

--- src/utils/fen.rs
+++ src/utils/fen.rs
@@ -2,7 +2,7 @@ use gamestate::Gamestate:: {
     Gamestate,
     Board,
 };
-
+use thiserror::Error;
 use core::str;
 
 pub const DEFAULT_FEN: &str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
