8abaca162ba9977d5bb29a8d7d261de9a8ec6de4
Tym Drogin <tim.drogin@gmail.com>
2024-05-19 23:39:10 +0100

    Stage

--- src/gamestate.rs
+++ src/gamestate.rs
@@ -1,17 +1,23 @@
-pub mod board;
 pub mod defs;
+pub mod board;
 
 use self::{
+    defs::{
+        Side,
+        CastlingRights,
+    },
     board::Board,
-    defs::{CastlingRights, Side},
 };
+
+
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Gamestate {
     pub board: Board,
-    pub side_to_move: Side,
-    pub castling_rights: CastlingRights,
+    pub side_to_move: Side, 
+    pub castling_rights: CastlingRights,   
     pub en_passant: Option<u8>,
     pub half_move_clock: usize,
     pub full_move_count: usize,
 }
+
--- src/gamestate/defs.rs
+++ src/gamestate/defs.rs
@@ -1,8 +1,3 @@
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum Side {
-    White,
-    Black,
-}
 // Instead of stantrart bitflags i decided to go with more ideomatic
 // way of encapsulating states using enums. The good thing is in the future
 // use of enums will pay of by forcing me or anybody else to cover all cases.
@@ -12,13 +7,13 @@ pub enum CastlingSide {
     None,
     Kingside,
     Queenside,
-    Both,
+    Both
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct CastlingRights {
-    white: CastlingSide,
-    black: CastlingSide,
+    pub white: CastlingSide,
+    pub black: CastlingSide,
 }
 impl CastlingRights {
     pub fn new(white_sides: CastlingSide, black_sides: CastlingSide) -> Self {
@@ -35,25 +30,17 @@ impl CastlingRights {
     }
 
     // NOTE: Do not pass None to cs
-    pub fn is_allowed(&self, side_to_mode: Side, cs: CastlingSide) -> bool {
+    pub fn is_allowed(&self, side_to_mode:Side, cs:CastlingSide) -> bool {
         match side_to_mode {
             Side::White => match cs {
-                CastlingSide::Kingside => {
-                    matches!(self.white, CastlingSide::Kingside | CastlingSide::Both)
-                }
-                CastlingSide::Queenside => {
-                    matches!(self.white, CastlingSide::Queenside | CastlingSide::Both)
-                }
+                CastlingSide::Kingside => matches!(self.white, CastlingSide::Kingside | CastlingSide::Both),
+                CastlingSide::Queenside => matches!(self.white, CastlingSide::Queenside | CastlingSide::Both),
                 CastlingSide::Both => self.white == CastlingSide::Both,
                 CastlingSide::None => false, // Castling None does not make sense to be allowed
             },
             Side::Black => match cs {
-                CastlingSide::Kingside => {
-                    matches!(self.black, CastlingSide::Kingside | CastlingSide::Both)
-                }
-                CastlingSide::Queenside => {
-                    matches!(self.black, CastlingSide::Queenside | CastlingSide::Both)
-                }
+                CastlingSide::Kingside => matches!(self.black, CastlingSide::Kingside | CastlingSide::Both),
+                CastlingSide::Queenside => matches!(self.black, CastlingSide::Queenside | CastlingSide::Both),
                 CastlingSide::Both => self.black == CastlingSide::Both,
                 CastlingSide::None => false, // Castling None does not make sense to be allowed
             },
