785bd7b9b1c37d2f9b9a2a946ac60b3d66a37b0b
Tym Drogin <tim.drogin@gmail.com>
2024-05-29 00:51:54 +0100

    Some tests

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ mod utils;
 use utils::fen::*;
 
 fn main() {
+    let fen = Fen("rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2".to_string());
+    let game = fen.process().unwrap();
+    print!("{}", game);
+
+    let fen = Fen("rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR b KQkq c6 0 2".to_string());
+    let game = fen.process().unwrap();
+    print!("{}", game);
+
     let fen = Fen(DEFAULT_FEN.to_string());
     let game = fen.process().unwrap();
     print!("{}", game);
