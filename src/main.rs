d1181edced145be355202819301358a46e7d8fd9
Tym Drogin <tim.drogin@gmail.com>
2025-07-06 21:16:17 +0100

    spme test code

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,11 @@ mod engine;
 
 use board::Side;
 use board::Square;
-use masks::BISHOP_RAYS;
-use masks::ROOK_RAYS;
-use movegen::defs::*;
+use movegen::constants::*;
 use movegen::*; // Assuming movegen is in your current crate or correctly referenced
 
 use gamestate::*;
+use gamestate::board::{PieceType, Bitboard};
 use utils::display::*;
 use utils::fen::*;
 
@@ -21,7 +20,7 @@ fn print_bitboard(bitboard: u64) {
         // Ranks from 7 to 0 (a8 to h8)
         for file in 0..8 {
             // Files from 0 to 7 (a to h)
-            let square_index = Square::new_from_file_rank(file, rank).unwrap().get_index();
+            let square_index = Square::new_from_file_rank(file, rank).get_index();
             let mask = 1u64 << square_index;
             let is_set = (bitboard & mask) != 0;
             print!("{} ", if is_set { '*' } else { '.' });
@@ -31,15 +30,16 @@ fn print_bitboard(bitboard: u64) {
 }
 
 fn main() {
-    let mut game: Gamestate = Fen(DEFAULT_FEN.to_string()).process().unwrap();
+    let pawn_double_push_fen: &str = "rnbqkbnr/8/8/p6p/1ppp4/4ppp1/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    let castling_fen: &str = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/R3K2R w KQkq - 0 1";
+    let mut game: Gamestate = Fen(castling_fen.to_string()).process().unwrap();
     let moves = MoveGen::new(&mut game).generate_moves();
-    print!("The move counter for knights is {}", moves.len());
+    print!("The total moves at this position is: {}", moves.len());
     println!();
-    for mov in &moves {
-        print!("{}", mov);
-        println!();
+    for chm in &moves {
+        println!("{}", chm);
     }
+    print!("King is at square {}" ,game.board.get_piece_at_square(Square::new(4)).unwrap().0 as usize);
 
-    println!("{:?}", game);
-    print!("{}", MoveDisplayWrapper(moves));
+    println!("{}", game);
 }
